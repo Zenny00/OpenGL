f14bea94af52f677e0841ebcd0972929c7d91b71
Zenny <joshuacomfort@gmail.com>
2023-09-03 10:22:58 -0400

    Initial commit. Window with simple triangle

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "glad.h"
+#include <GLFW/glfw3.h>
+
+int main() {
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    // Load OpenGL
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Could not load OpenGL" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.75f, 0.50f, 0.25f, 1.0f);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
